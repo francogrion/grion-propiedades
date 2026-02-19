e492adbc631c233fb23a101719fd88f9bbaca49a
francogrion <franco.grion@gmail.com>
2026-02-19 16:25:02 +0100

    Add router and fix get property by title query

---
+++ src/api/router.rs
@@ -0,0 +1,17 @@
+use axum::{ Router, routing::get };
+
+use crate::application::queries::{
+    get_all_properties_query::get_all_properties_query,
+    get_property_by_id_query::get_property_by_id_query,
+    get_property_by_title_query::get_property_by_title_query,
+};
+
+use super::health_checker_handler;
+
+pub fn create_router() -> Router {
+    Router::new()
+        .route("/api/healthchecker", get(health_checker_handler))
+        .route("/api/properties", get(get_all_properties_query))
+        .route("/api/properties/{id}", get(get_property_by_id_query))
+        .route("/api/properties/{name}", get(get_property_by_title_query))
+}
--- src/application/queries/get_property_by_title_query.rs
+++ src/application/queries/get_property_by_title_query.rs
@@ -4,21 +4,31 @@ use serde_json::json;
 
 use crate::data::repositories::property_repository::PropertyRepository;
 
-pub async fn get_property_by_title_query(Path(title): Path<String>) -> Result<
+use axum::extract::Query;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct PropertyByTitleQuery {
+    pub title: String,
+}
+
+pub async fn get_property_by_title_query(Query(
+    params,
+): Query<PropertyByTitleQuery>) -> Result<
     impl IntoResponse,
     (StatusCode, Json<serde_json::Value>)
 > {
     let repository = PropertyRepository::new();
-    debug!("Fetching Property with title = {}", title);
+    debug!("Fetching Property with title = {}", params.title);
 
-    match repository.get_by_title(title.clone()).await {
+    match repository.get_by_title(params.title.clone()).await {
         Ok(property) => Ok((StatusCode::OK, Json(property))),
         Err(_) =>
             Err((
                 StatusCode::NOT_FOUND,
                 Json(
                     json!({
-                "error": format!("Property with title {} not found", title)
+                "error": format!("Property with title {} not found", params.title)
             })
                 ),
             )),
