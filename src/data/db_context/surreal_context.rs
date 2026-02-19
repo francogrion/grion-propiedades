582f9e10df7af38c14dcb908eea6ac3bbd0b7031
francogrion <franco.grion@gmail.com>
2026-02-19 11:45:05 +0100

    Save files with new editorconfig

--- src/data/db_context/surreal_context.rs
+++ src/data/db_context/surreal_context.rs
@@ -1,12 +1,18 @@
 use log::debug;
 use once_cell::sync::Lazy;
-use surrealdb::{ Result, Surreal, engine::{ any, remote::ws::Client }, opt::auth::Root };
+use surrealdb::{
+    Result,
+    Surreal,
+    engine::{ any, remote::ws::Client },
+    opt::auth::Root,
+};
 
 pub static DB: Lazy<Surreal<Client>> = Lazy::new(Surreal::init);
 
 pub async fn connect_db() -> Result<()> {
     // URL de tu instancia en Surreal Cloud
-    let url = "wss://grion-propiedad-06e737tn75r1j97flkva6ns3dg.aws-euw1.surreal.cloud";
+    let url =
+        "wss://grion-propiedad-06e737tn75r1j97flkva6ns3dg.aws-euw1.surreal.cloud";
     let db = any::connect(url).await?;
 
     let _ = db.signin(Root {
