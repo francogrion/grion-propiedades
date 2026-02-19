e41771017e8c38614349b50be579972e14e44906
francogrion <franco.grion@gmail.com>
2026-02-19 12:43:27 +0100

    Fix DB connection

--- src/data/db_context/surreal_context.rs
+++ src/data/db_context/surreal_context.rs
@@ -1,25 +1,27 @@
 use log::debug;
 use once_cell::sync::Lazy;
-use surrealdb::{
-    Result,
-    Surreal,
-    engine::{ any, remote::ws::Client },
-    opt::auth::Root,
-};
+use surrealdb::{ Result, Surreal, engine::any::Any, opt::auth::Root };
 
-pub static DB: Lazy<Surreal<Client>> = Lazy::new(Surreal::init);
+pub static DB: Lazy<Surreal<Any>> = Lazy::new(Surreal::init);
 
 pub async fn connect_db() -> Result<()> {
     // URL de tu instancia en Surreal Cloud
     let url =
         "wss://grion-propiedad-06e737tn75r1j97flkva6ns3dg.aws-euw1.surreal.cloud";
-    let db = any::connect(url).await?;
 
-    let _ = db.signin(Root {
+    // Conectar usando el engine Any (acepta ws, wss, http, https)
+    DB.connect(url).await?;
+
+    // Autenticación
+    DB.signin(Root {
         username: "gp-admin".to_string(),
         password: "GrionPropiedadesAdmin11".to_string(),
-    }).await;
-    let _ = db.use_ns("gp-dev").use_db("main").await?;
+    }).await?;
+
+    // Seleccionar namespace y database
+    DB.use_ns("gp-dev").use_db("main").await?;
+
     debug!("Connected to SurrealDB 🗄️");
+
     Ok(())
 }
