b9b45909fb6c940abfd504f0fe117a09cbb4c9e0
francogrion <franco.grion@gmail.com>
2026-02-20 16:38:25 +0100

    Fix property model to retrieve from DB

--- src/domain/models/image.rs
+++ src/domain/models/image.rs
@@ -1,7 +1,8 @@
 use serde::{ Deserialize, Serialize };
-use surrealdb::types::SurrealValue;
+use surrealdb::types::{ RecordId, SurrealValue };
 
 #[derive(Clone, Debug, Deserialize, Serialize, SurrealValue)]
 pub(crate) struct Image {
+    pub(crate) id: Option<RecordId>,
     pub(crate) url: String,
 }
--- src/domain/models/property.rs
+++ src/domain/models/property.rs
@@ -2,8 +2,6 @@ use chrono::{ DateTime, Utc };
 use serde::{ Deserialize, Serialize };
 use surrealdb::types::{ RecordId, SurrealValue };
 
-use super::{ estado::Estado, image::Image };
-
 #[derive(Clone, Debug, Deserialize, Serialize, SurrealValue)]
 pub(crate) struct Property {
     pub(crate) id: Option<RecordId>,
@@ -12,14 +10,13 @@ pub(crate) struct Property {
     pub(crate) created_at: Option<DateTime<Utc>>,
     pub(crate) currency: String,
     pub(crate) description: String,
-    pub(crate) estado: Estado,
+    pub(crate) estado: RecordId,
     pub(crate) featured: bool,
-    pub(crate) image: Option<Image>,
-    pub(crate) images: Vec<Image>,
+    pub(crate) images: Option<Vec<RecordId>>,
     pub(crate) location: String,
     pub(crate) operation: String,
     pub(crate) price: f64,
     pub(crate) surface_m2: f64,
     pub(crate) title: String,
-    pub(crate) type_: String,
+    pub(crate) property_type: String,
 }
